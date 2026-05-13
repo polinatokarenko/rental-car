02c7f239f19412fe202ae088b0104be1bc23cda7
Polina <yuriinnx@gmail.com>
2026-05-14 02:20:52 +0300

    complete full homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import css from "./page.module.css"
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <section>
-      <div>
-        <h1>Find your perfect rental car</h1>
-        <h2>Reliable and budget-friendly rentals for any journey</h2>
+    <section className={css.homeContainer}>
+      <div className={css.homeContent}>
+        <div className={css.homeTextContainer}>
+          <h1>Find your perfect rental car</h1>
+          <h2>Reliable and budget-friendly rentals for any journey</h2>
+        </div>
+        <Link href="/catalog">
+          <button className={css.homeButton}>View Catalog</button>
+        </Link>
       </div>
-      <button>View Catalog</button>
     </section>
   )
 }
